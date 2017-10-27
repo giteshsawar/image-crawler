aad5cebd177b7925834b20f09d416484656f065a
giteshsawar <gitesh.sawariya@gmail.com>
2017-10-27 06:30:28 -0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@
         
         $scope.getImage = function() {
             
+            $scope.message = "";
             $scope.loading = true;
             $scope.images = [];
             socket.emit('getimage', $scope.url);
@@ -24,8 +25,18 @@
             $scope.url = data.url;
             $scope.images = [];
             console.log($scope.images);
+            if(data.images.length == 0) {
+                $scope.message = 'No images retrieved';
+                return;
+            }
             for(var i=0, x=data.images.length; i<x; i++) {
-                if(data.images[i].indexOf('http') == -1) {
+                if(data.images[i].indexOf('http') == -1 && data.images[i].indexOf('data:image') == -1) {
+                    
+                    if(data.images[i][0] == '/') {
+                        data.images[i] = data.images[i].substr(1);
+                        console.log(data.images[i]);
+                    }
+                    
                     $scope.images.push(data.url + '/' +data.images[i]);
                 }
                 else {
